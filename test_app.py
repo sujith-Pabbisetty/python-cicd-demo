b1b3756a02a5d0cb6358d1d62a4efc816e45ad88
sujith-Pabbisetty <sujithpabbisetty@gmail.com>
2026-09-22 15:25:32 +0530

    2nd commit for app and test_app.py files to update the message in home route and test case for the same.

--- test_app.py
+++ test_app.py
@@ -6,8 +6,8 @@ def client():
     app.config["TESTING"] = True
     with app.test_client() as client:
         yield client
-
+        
 def test_home_route(client):
     response = client.get("/")
     assert response.status_code == 200
-    assert b"Hello World!" in response.data
+    assert b"Automatic End-to-End CI/CD is fully working!" in response.data
