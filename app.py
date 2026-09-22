d3a3c040d87c96019e182741c9dd454ce883c511
sujith-Pabbisetty <sujithpabbisetty@gmail.com>
2026-09-22 15:21:47 +0530

    2nd commit by changing the message in app.py file

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World! Python CI/CD Pipeline is working perfectly!", 200
+    return "Hello World! Automatic End-to-End CI/CD is fully working!", 200
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
